ffd086465e23437dd65090df4e77443a3bd033cd
tannerlsmith <tanner.smith@uvm.edu>
2021-07-11 11:42:15 -0400

    added writeFile, request not defined

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const fs = require('fs');
-const { nanoid } = require('nanoid')
-var ID = nanoid();
+const { v1: uuidv1 } = require('uuid');
+
+// 
+
 
 router.get('/api/notes', (req, res) =>{
     console.log('in the get route')
@@ -12,24 +14,24 @@ router.get('/api/notes', (req, res) =>{
 })
 
 router.post('/api/notes', (req, res) => {
-    console.log('in the post route')
-    fs.readFile('./db/db.json', (err, data) =>{
-        if (err) throw err;
-        res.send(data)
-    })
-    // Already have data, need to add more stuff to data.
-    // have to re-write db.json
-    // writefile
-    
-    // Am I writing this in the correct spot?
-    fs.writeFile('./db/db.json' ({ body }, data)
+    // for new notes
+    const newNote = request.body;
 
-    )
-    
+    // for newNote id
+    console.log('\n\nPOST request - New Note : ' + JSON.stringify(newnote));
+    newNote.id = uuidv1();
+    let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
-
+    // Writes new note
+    data.push(newNote);
+    fs.writeFileSync('./db.db.json', JSON.stringify(data));
+    console.log("\nSuccessfully added new note to 'db.json' file");
+    response.json(data);
 
 
 })
 
 module.exports = router
+
+
+
